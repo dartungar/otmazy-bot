f6c41346c481f9e0e9dd68ace3547cc841946a54
dartungar <dartungar@gmail.com>
2019-11-30 14:21:08 +0300

    working version v0.1

--- bot.py
+++ bot.py
@@ -1,70 +1,108 @@
+import logging
+import os
+import db
 import pandas as pd
 import pymorphy2
 import random
-import template
-from construct import constructor
-#from words import get_podlezh, get_skaz, get_noun_dop, get_noun_obst, get_predlog, get_must
+import test
+from test import test_constructor
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 
 
-def get_form(word, morph, case):
-    parsed = morph.parse(word)[0]
-    morphed = parsed.inflect({case})[0]
-    return morphed
+BOT_TOKEN = os.environ.get('BOT_TOKEN_OTMAZY')
+
+df = pd.read_excel('otmazy_words.xlsx', index_col=0, sheet_name=None)
+logger.info('loaded data from excel')
+morph = pymorphy2.MorphAnalyzer()
+logger.info('initialized Morph')
+
+keyboard = ReplyKeyboardMarkup([['/start', '/generate']], True)
 
 
-def create_random_template():
-    number = random.randint(1, 4)
-    if number == 1:
-        tmplt = template.Template_1(words=df, morph=morph)
-    if number == 2:
-        tmplt = template.Template_2(words=df, morph=morph)
-    if number == 3:
-        tmplt = template.Template_3(words=df, morph=morph)
-    if number == 4:
-        tmplt = template.Template_4(words=df, morph=morph)
-
-    return (number, tmplt)
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
 
-def test_constructor(words, morph):
-    tense = 'futr' # TODO: разобраться почему не работает, cейчас везде прошлое о_О
-    subjim = random.randint(0, 1)
-    obj_type = random.choice(['person', 'project', 'thing'])
-    has_pred_spice = random.randint(0, 1)
-    beginning = random.randint(0, 1)
-    if obj_type == 'project':
-        adv_type = random.choice(['person', 'event'])
-    else:
-        adv_type = None
-    ending = False if subjim else True
+def start(update, context):
+    username = update.message.from_user.username
 
-    return constructor(words=words, 
-                        morph=morph, 
-                        tense=tense, 
-                        subject_is_myself=subjim, 
-                        has_predicate_spice=has_pred_spice,
-                        object_type=obj_type,
-                        adv_type=adv_type, 
-                        has_beginning=beginning, 
-                        has_ending=ending)
+    reply_text = f''' Otmazy Bot v 0.1
+    Привет, {username}!
+    Я - альфа-версия бота для генерации отмазок.
+    Сейчас я могу генерировать полуосмысленные (зато забавные) отмазки.
+    Потом поумнею и начну выдавать что-то, похожее на реальность.
+    Нажми /generate, чтобы сгенерировать отмазку.
+    '''
+    update.message.reply_text(reply_text, reply_markup=keyboard)
+
+def generate(update, context):
+    try:
+        text = test_constructor(words=df, morph=morph)
+        #text = random.randint(1, 10)
+        logger.info('generated text')
+    except:
+        text = 'whoops'
+        logger.warning('failed to generate text')
+    #text = 'a reply'
+    update.message.reply_text(text, reply_markup=keyboard)
+
+
+def main():
+
+
+
+    updater = Updater(BOT_TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+
+    # convhandler = ConversationHandler(
+    #     entry_points=[
+    #                 CommandHandler('start', start),
+    #                 CommandHandler('отмазка', ask_notion_api_key),
+    #                 ],
+
+    #     states={
+    #         TYPING_NOTION_API_KEY: [MessageHandler(Filters.text, set_notion_api_key)],
+    #         TYPING_NOTION_PAGE_ADDRESS: [MessageHandler(Filters.text, set_page_address)],
+    #     },
+
+    #     fallbacks=[CommandHandler('done', done)],
+    #     name='my_conversation',
+    #     persistent=False
+    # )
+
+    # dp.add_handler(convhandler)
+
+    start_handler = CommandHandler('start', start)
+    dp.add_handler(start_handler)
+
+    generate_v0_handler = CommandHandler('generate', generate)
+    dp.add_handler(generate_v0_handler)
+
+    # check_client_handler = CommandHandler('check_client', check_client)
+    # dp.add_handler(check_client_handler)
+
+    # check_page_handler = CommandHandler('check_page', check_page)
+    # dp.add_handler(check_page_handler)
+
+    # send_text_to_notion_handler = MessageHandler(Filters.text, send_text_to_notion)
+    # dp.add_handler(send_text_to_notion_handler)
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
 
 
 if __name__ == '__main__':
-
-    df = pd.read_excel('otmazy_words.xlsx', index_col=0, sheet_name=None)
-    morph = pymorphy2.MorphAnalyzer()
-
-    for i in range(10):
-        #temp = create_random_template()
-        #text = f'{str(temp[0])} {temp[1].text}'
-        #str(create_random_template()[0]) +' '+  create_random_template()[1].text
-        text = test_constructor(words=df, morph=morph)
-        print(text)
-
-
-
-
-
-
-
+    main()
---
+++ test.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import pymorphy2
+import random
+import template
+from construct import constructor
+#from words import get_podlezh, get_skaz, get_noun_dop, get_noun_obst, get_predlog, get_must
+
+
+
+def get_form(word, morph, case):
+    parsed = morph.parse(word)[0]
+    morphed = parsed.inflect({case})[0]
+    return morphed
+
+
+def create_random_template():
+    number = random.randint(1, 4)
+    if number == 1:
+        tmplt = template.Template_1(words=df, morph=morph)
+    if number == 2:
+        tmplt = template.Template_2(words=df, morph=morph)
+    if number == 3:
+        tmplt = template.Template_3(words=df, morph=morph)
+    if number == 4:
+        tmplt = template.Template_4(words=df, morph=morph)
+
+    return (number, tmplt)
+
+
+def test_constructor(words, morph):
+    print('initializing test_constructor...')
+    tense = 'futr' # TODO: разобраться почему не работает, cейчас везде прошлое о_О
+    subjim = random.randint(0, 1)
+    obj_type = random.choice(['person', 'project', 'thing'])
+    has_pred_spice = random.randint(0, 1)
+    beginning = random.randint(0, 1)
+    if obj_type == 'project':
+        adv_type = random.choice(['person', 'event'])
+    else:
+        adv_type = None
+    ending = False if subjim else True
+
+    print('constructor parameters set')
+
+    text = constructor(words=words, 
+                        morph=morph, 
+                        tense=tense, 
+                        subject_is_myself=subjim, 
+                        has_predicate_spice=has_pred_spice,
+                        object_type=obj_type,
+                        adv_type=adv_type, 
+                        has_beginning=beginning, 
+                        has_ending=ending)
+
+    print('generated non-prettified text')
+
+    text = text.replace('  ', ' ').strip().capitalize()
+
+    print('prettified text')
+
+    return text
+
+
+if __name__ == '__main__':
+
+    df = pd.read_excel('otmazy_words.xlsx', index_col=0, sheet_name=None)
+    morph = pymorphy2.MorphAnalyzer()
+
+    for i in range(10):
+        #temp = create_random_template()
+        #text = f'{str(temp[0])} {temp[1].text}'
+        #str(create_random_template()[0]) +' '+  create_random_template()[1].text
+        text = test_constructor(words=df, morph=morph)
+        print(text)
+
+
+
+
+
+
+
