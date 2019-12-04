24f856e36164112057d10c6eb39a3b73fb928bfa
dartungar <dartungar@gmail.com>
2019-12-04 22:21:45 +0300

    v 0.2

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ def error(update, context):
 def start(update, context):
     username = update.message.from_user.username
 
-    reply_text = f''' Otmazy Bot v 0.1
+    reply_text = f''' Otmazy Bot v 0.2 alpha
     Привет, {username}!
     Я - альфа-версия бота для генерации отмазок.
     Сейчас я могу генерировать полуосмысленные (зато забавные) отмазки.
