1f2f195a3cb98729a5bb1e2960baac8adff74f2b
dartungar <dartungar@gmail.com>
2019-12-15 22:14:54 +0300

    fixed typo in /start

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ def error(update, context):
 def start(update, context):
     username = update.message.from_user.username
 
-    reply_text = f''' Otmazy Bot v 0.5.0 aplha
+    reply_text = f''' Otmazy Bot v 0.5.0 alpha
     Привет, {username}!
     Я - альфа-версия бота для генерации отмазок.
     Сейчас я могу генерировать полуосмысленные (зато забавные) отмазки.
